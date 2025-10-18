8e4ddb0ca4d5ac1b2c6ab484ac44a418d622f9cc
Tauan Pastana <wendreotauan77@gmail,com>
2025-10-17 21:50:40 -0300

    Fix: correção em importação de módulo

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -1,8 +1,8 @@
 from .Sites import Sites
 from .G1 import G1
-from .CNN import CNN
+from .cnn import CNN
 from.band import Band
 
 from.uol import Uol
 
-__all__ = ['Sites', 'G1', 'CNN', 'Uol', 'Band']
+__all__ = ['Sites', 'G1', 'cnn', 'Uol', 'Band']
