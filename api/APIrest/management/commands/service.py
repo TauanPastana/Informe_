16721d6a875d9a51e4934affaa1346c8ebb31d41
Tauan Pastana <wendreotauan77@gmail,com>
2025-12-21 22:56:28 -0300

    feat(service.py): integração da raspagem da cnn para o salvamento no banco de dados

--- api/APIrest/management/commands/service.py
+++ api/APIrest/management/commands/service.py
@@ -1,14 +1,16 @@
-from ...models import News_Informe
-from src.classes.G1 import noticias_G1
 from django.core.management.base import BaseCommand
 
-class Command(BaseCommand):
+from ...models import News_Informe
+from src.classes.G1 import noticias_G1
+from src.classes.cnn import noticias_CNN
 
+
+class Command(BaseCommand):
     def handle(self, *args, **options):
-        lista_de_dicts = noticias_G1()
-        for dados in lista_de_dicts:
+        for dados in noticias_G1() + noticias_CNN():
             News_Informe.objects.update_or_create(
                 url=dados["url"],
                 defaults=dados,
             )
+
     
